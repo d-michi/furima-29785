b4d0383389aa9eb93d8a1015c6e37a5d106ad211
d-michi <dmichi.gk@gmail.com>
2020-09-20 17:51:04 +0900

    ユーザー管理機能の修正4

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,11 @@ class User < ApplicationRecord
   validates :nickname, :email, :password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date, presence: true
   
 
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角文字を使用してください' } do
-    validates :first_name
-    validates :last_name
-    validates :first_name_kana
-    validates :last_name_kana
+  with_options presence: true do
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角文字を使用してください'}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角文字を使用してください'}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角文字を使用してください'}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角文字を使用してください'}
   end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
-    password_confirmation { password }
+    password              { 'abc111' }
+    password_confirmation { 'abc111' }
     first_name            { '山田' }
     first_name_kana       { 'カタカナ' }
     last_name             { '太郎' }
