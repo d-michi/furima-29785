838a7f2a600e40d236cbae6ef84f665f8e07eeb3
d-michi <dmichi.gk@gmail.com>
2020-09-27 18:50:12 +0900

    出品画像の表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
  belongs_to_active_hash :item_scheduled_delivery
  has_one_attached :image
 
- validates :item_name, :item_explain, :price, :prefecture_id, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_scheduled_delivery_id, presence: true
+ validates :image, :item_name, :item_explain, :price, :prefecture_id, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_scheduled_delivery_id, presence: true
 
  with_options numericality: { other_than: 0 } do
   validates :prefecture_id, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_scheduled_delivery_id
