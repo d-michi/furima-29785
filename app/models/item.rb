a31f508e31918d02b2ea6dce9e38d3ecc90bdf2a
d-michi <dmichi.gk@gmail.com>
2020-09-30 16:57:33 +0900

    単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,16 @@ class Item < ApplicationRecord
  has_one :order
  belongs_to :user
 
+ validates :image,                         presence: true
+ validates :item_name,                     presence: true
+ validates :item_explain,                  presence: true
+ validates :price,                         presence: true
+ validates :prefecture_id,                 presence: true
+ validates :item_category_id,              presence: true
+ validates :item_sales_status_id,          presence: true
+ validates :item_shipping_fee_status_id,   presence: true
+ validates :item_scheduled_delivery_id,    presence: true
+
  extend ActiveHash::Associations::ActiveRecordExtensions
  belongs_to_active_hash :prefecture
  belongs_to_active_hash :item_category
@@ -10,9 +20,8 @@ class Item < ApplicationRecord
  belongs_to_active_hash :item_scheduled_delivery
  has_one_attached :image
 
- validates :image, :item_name, :item_explain, :price, :prefecture_id, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_scheduled_delivery_id, presence: true
-
  with_options numericality: { other_than: 0 } do
   validates :prefecture_id, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_scheduled_delivery_id
  end
+ validates :price, numericality:{greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
-  end
+    item_name                    { '商品名' }
+    item_explain                 { '商品説明' }
+    price                        { '77777' }
+    prefecture_id                { '1' }
+    item_category_id             { '2' }
+    item_sales_status_id         { '1' }
+    item_shipping_fee_status_id  { '2' }
+    item_scheduled_delivery_id   { '1' }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+   end
 end
