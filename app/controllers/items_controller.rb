5503d0816519d873e9734551fa1ae81d9604b2d7
d-michi <dmichi.gk@gmail.com>
2020-10-02 20:50:38 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
@@ -30,7 +30,6 @@ class ItemsController < ApplicationController
     @user = User.find(params[:id])
   end
 
-
   private
 
   def item_params
@@ -38,8 +37,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in?
   end
 end
